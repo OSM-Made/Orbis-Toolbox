acd4654a244499858211a7df33cfb3797ec6ba18
Greg <oldskoolmodz@gmail.com>
2021-05-11 09:33:08 -0600

    Set up initial project

---
+++ Orbis Toolbox/Common.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <map>
+#include <vector>
+#include <string>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <orbis/libkernel.h>
+#include <orbis/libmonovm.h>
+
+#include "Utilities.h"
+#include "Mono.h"
+#include "Detour.h"
+#include "Patcher.h"
+#include "UI.h"
